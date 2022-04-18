762c711b09fb3279d6e2bd0fc65568449b694786
pdt64 <piolot64@gmail.com>
2022-04-17 23:22:16 -0500

    Created views for checkout

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "2.7.1"
 gem 'sassc-rails'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.2", ">= 7.0.2.3"
+#gem "rails", "~> 7.0.2", ">= 7.0.2.3"
+gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
@@ -87,6 +88,8 @@ group :production do
   gem 'pg'
 
 end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise', '~> 4.8', '>= 4.8.1'
 
--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,13 +1,30 @@
 class CheckoutController < ApplicationController
   def create
-    @record = Record.find(params[:id])
-    if @record.nil?
+    record = Record.find(params[:id])
+    if record.nil?
       redirect_to root_path
-      nil
+      return
     end
 
     respond_to do |format|
       format.js
     end
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      success_url:          checkout_create_url,
+      cancel_url:           checkout_cancel_url,
+      line_item:            [
+        name:        record.name,
+        description: record.description,
+        amount:      (record.price * 100).to_i,
+        currency:    "cad",
+        quantity:    1
+      ]
+    )
   end
+
+  def success; end
+
+  def cancel; end
 end
